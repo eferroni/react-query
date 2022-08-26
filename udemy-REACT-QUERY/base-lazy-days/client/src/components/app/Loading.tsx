be2b7defda1daba0f192052b0d68fb1f6dbef0db
eduardo-ferroni <eduard.ferroni@hp.com>
2022-08-26 16:29:46 -0300

    finish section 8

--- udemy-REACT-QUERY/base-lazy-days/client/src/components/app/Loading.tsx
+++ udemy-REACT-QUERY/base-lazy-days/client/src/components/app/Loading.tsx
@@ -1,11 +1,12 @@
 import { Spinner, Text } from '@chakra-ui/react';
 import { ReactElement } from 'react';
-import { useIsFetching } from 'react-query';
+import { useIsFetching, useIsMutating } from 'react-query';
 
 export function Loading(): ReactElement {
   const isFetching = useIsFetching(); // for now, just don't display: ;
+  const isMutating = useIsMutating(); // for now, just don't display: ;
 
-  const display = isFetching ? 'inherit' : 'none';
+  const display = isFetching || isMutating ? 'inherit' : 'none';
 
   return (
     <Spinner
